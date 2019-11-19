9501f005906f7f093784f80fe5c7d4bcb6dfa945
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-19 04:03:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -10,9 +10,7 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+   
       
     elsif req.path.match(/cart/)
       if @@cart.empty?
@@ -22,6 +20,9 @@ class Application
           resp.write "#{cart_item}\n"
         end
       end
+       elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
     else
       resp.write "Path Not Found"
     end
